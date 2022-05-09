8e043a822548ccaf2f67f03091463252311b7bfa
AntonKulagin <i-gorusich@mail.ru>
2022-05-09 13:57:28 +0300

    homework done, corrected

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let temperatureInCelsius = prompt('Введите температуру в градусах Цельсия');
 
 temperatureInCelsius = Number(temperatureInCelsius)
-console.log(typeof temperatureInCelsius)
+console.log('Тип данных:', typeof temperatureInCelsius)
 
 if (temperatureInCelsius === 0) {
 	alert('0 градусов по Цельсию - это температура замерзания воды')
@@ -9,6 +9,7 @@ if (temperatureInCelsius === 0) {
 	alert('Для замерзания воды температура должна быть 0 градусов по Цельсию либо ниже');
 }
 
-const temperatureInFahrenheit = temperatureIncelsius * 9 / 5 + 32;
+const temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32;
+console.log(temperatureInCelsius, 'Не соблюдено правило написания lowCamelCase')
 alert(`${temperatureInCelsius} градусов по Цельсию - это ${temperatureInFahrenheit} по 
 Фаренгейту.`);
