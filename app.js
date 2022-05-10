09f421a4e84c338ae672c074ce6320ea13c2addd
AntonKulagin <i-gorusich@mail.ru>
2022-05-10 20:02:18 +0300

    task 9.6 is done

---
+++ app.js
@@ -0,0 +1,26 @@
+const numbers = [10, 4, 100, -5, 54, 2]
+let sum = 0
+
+for (let i = 0; i < numbers.length; i++) {
+	sum += numbers[i] ** 3
+}
+console.log(sum)
+//========================================
+sum = 0
+
+for (let number of numbers) {
+	sum += number ** 3
+}
+console.log(sum)
+//========================================
+sum = 0
+
+numbers.forEach(number => sum += number ** 3)
+
+console.log(sum)
+//========================================
+sum = 0
+
+sum = numbers.reduce((acc, number) => acc += number ** 3, 0)
+
+console.log(sum)
