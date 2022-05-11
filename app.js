eb9bb42e3027c7a426c0e56ed7b6b9aad5e12cc8
AntonKulagin <i-gorusich@mail.ru>
2022-05-11 21:37:28 +0300

    task 9.1 corrected

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-let peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб']
+const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария', 'Светлана', 'Артем', 'Глеб']
 
-const personBeforeDinner = 'Кирилл'
-const personBeforeDinnerIndex = peopleWaiting.indexOf(personBeforeDinner)
+const personBeforeDinner = prompt('Введите имя последнего человека, которого успеет обслужить почта:')
 
-let newPeopleWaiting = [...peopleWaiting]
 
-peopleWaiting.forEach((person, index) => {
-	index <= personBeforeDinnerIndex
-		? giveParcel(person)
-		: leaveQueueWithoutParcel(person)
-})
-
+while (peopleWaiting.length > 0) {
+	peopleWaiting.includes(personBeforeDinner)
+		? giveParcel(peopleWaiting[0])
+		: leaveQueueWithoutParcel(peopleWaiting[0])
+}
 
 function giveParcel(name) {
-	newPeopleWaiting.shift()
-	alert(`${name} получил(а) посылку. В очереди осталось ${newPeopleWaiting.length} человек`)
+	peopleWaiting.shift()
+	alert(`${name} получил(а) посылку. В очереди осталось ${peopleWaiting.length} человек`)
 }
 
 function leaveQueueWithoutParcel(name) {
-	newPeopleWaiting.shift()
-	alert(`${name} не получил(а) посылку и ушел из очереди. В очереди осталось ${newPeopleWaiting.length} человек`)
+	peopleWaiting.shift()
+	alert(`${name} не получил(а) посылку и ушел из очереди. В очереди осталось ${peopleWaiting.length} человек`)
 }
