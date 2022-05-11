9cce43fd388aecfee18d74e4557670b8409238c4
AntonKulagin <i-gorusich@mail.ru>
2022-05-11 19:31:38 +0300

    corrected forOf

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let sum = 0
 // Через цикл for
 for (let i = 0; i < numbers.length; i += 1) {
 
-	console.log(`Не стоит изменять переменную number[${i}]:(${numbers[i]}) в массиве [${numbers}]`)
+	console.log(`Операции над переменной number[${i}]:(${numbers[i]}) приводит к мутации массива [${numbers}]`)
 	sum += numbers[i] ** 3
 }
 console.log(sum) // 1158411
@@ -14,7 +14,8 @@ console.log(sum) // 1158411
 sum = 0
 for (let num of numbers) {
 
-	console.log(`Не стоит изменять переменную num:(${num}) в массиве [${numbers}]`)
-	sum += num ** 3
+	num = num ** 3
+	sum += num
+	console.log(`В цикле ForOf изменение переменной num:(${num}) не ведет к мутации массива [${numbers}]`)
 }
 console.log(sum) // 1003904306910622800  // 1158411
