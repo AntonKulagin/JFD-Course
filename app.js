bade3140803d46603a0c91ccaa3aa5a5d81dc18a
AntonKulagin <i-gorusich@mail.ru>
2022-05-11 17:35:54 +0300

    corrected

--- app.js
+++ app.js
@@ -3,24 +3,18 @@ let sum = 0
 
 // Через цикл for
 for (let i = 0; i < numbers.length; i += 1) {
-	numbers[i] = numbers[i] ** 3
-	sum += numbers[i]
+
+	console.log(`Не стоит изменять переменную number[${i}]:(${numbers[i]}) в массиве [${numbers}]`)
+	sum += numbers[i] ** 3
 }
 console.log(sum) // 1158411
 
 
 // Через цикл for of
-console.log(numbers)
-
-console.log(`Необходимо привести массив к исходному состоянию`)
-for (let i = 0; i < numbers.length; i += 1) {
-	numbers[i] = Math.cbrt(numbers[i])
-}
-
-console.log(numbers)
 sum = 0
 for (let num of numbers) {
-	num = num ** 3
-	sum += num
+
+	console.log(`Не стоит изменять переменную num:(${num}) в массиве [${numbers}]`)
+	sum += num ** 3
 }
 console.log(sum) // 1003904306910622800  // 1158411
