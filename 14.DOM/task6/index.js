a121de660092b6e70f09451fccee6e8275b18fde
AntonKulagin <i-gorusich@mail.ru>
2022-06-02 17:07:17 +0300

    corrected task dom

--- 14.DOM/task6/index.js
+++ 14.DOM/task6/index.js
@@ -1,4 +1,4 @@
-const tasks = [
+let tasks = [
 	{
 		id: "1138465078061",
 		completed: false,
@@ -20,47 +20,52 @@ const taskList = document.querySelector('.tasks-list')
 let darkTheme = false
 const body = document.body
 
-addTask()
+showTask()
 addTheme()
 
 
-function addTask() {
+function showTask() {
 	taskList.innerHTML = ''
 
 	tasks.forEach(item => {
 		const taskId = item.id
 		const taskText = item.text
 
-		taskList.innerHTML += `
-		<div class="task-item" data-task-id="${taskId}">
-			<div class="task-item__main-container">
-				<div class="task-item__main-content">
-						<form class="checkbox-form">
-							<input class="checkbox-form__checkbox" type="checkbox" id="${taskId}">
-							<label for="${taskId}"></label>
-						</form>
-						<span class="task-item__text">
-							${taskText}
-						</span>
-				</div>
-				<button class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
-						Удалить
-				</button>
-			</div>
-		</div>
-	`
+		taskList.insertAdjacentHTML('beforeend', createTask(taskId, taskText))
 	})
+	addTheme()
+}
+
+function createTask(id, text) {
+	return `
+	<div class="task-item" data-task-id="${id}">
+		<div class="task-item__main-container">
+			<div class="task-item__main-content">
+					<form class="checkbox-form">
+						<input class="checkbox-form__checkbox" type="checkbox" id="${id}">
+						<label for="${id}"></label>
+					</form>
+					<span class="task-item__text">
+						${text}
+					</span>
+			</div>
+			<button class="task-item__delete-button default-button delete-button" data-delete-task-id="5">
+					Удалить
+			</button>
+		</div>
+	</div>
+`
 }
 
 
-const createTask = document.querySelector('.create-task-block')
+const formCreateTaskBlock = document.querySelector('.create-task-block')
 
 const spanErrorText = document.createElement('span')
 spanErrorText.className = 'error-message-block'
 
 
 
-createTask.addEventListener('submit', (event) => {
+formCreateTaskBlock.addEventListener('submit', (event) => {
 	event.preventDefault()
 	spanErrorText.remove()
 
@@ -68,75 +73,95 @@ createTask.addEventListener('submit', (event) => {
 	const taskNameInput = target.taskName
 	const inputValue = taskNameInput.value
 
-	if (inputValue) {
-		let hasSimilarTask = false
+	addDataToTaskArray(inputValue)
+	taskNameInput.value = ''
+	showTask()
+})
 
-		tasks.forEach(item => {
-			if (item.text === inputValue) {
-				spanErrorText.textContent = 'Задача с таким названием уже существует.'
-				createTask.append(spanErrorText)
-				hasSimilarTask = true
+
+function addDataToTaskArray(inputValue) {
+	if (inputValue && !hasSimilarTask(inputValue)) {
+
+		tasks.push(
+			{
+				id: `${Date.now()}`,
+				completed: false,
+				text: inputValue
 			}
-		})
+		)
+	} else {
+		showMassegeError(inputValue)
+	}
+}
 
-		if (!hasSimilarTask) {
-			tasks.push(
-				{
-					id: `${Date.now()}`,
-					completed: false,
-					text: inputValue
-				}
-			)
-			addTask()
-			addTheme()
-		}
+function hasSimilarTask(inputValue) {
+	return tasks.filter(item => item.text === inputValue).length
+}
+
+function showMassegeError(inputValue) {
+	if (inputValue) {
+		spanErrorText.textContent = 'Задача с таким названием уже существует.'
 	} else {
 		spanErrorText.textContent = 'Название задачи не должно быть пустым'
-		createTask.append(spanErrorText)
 	}
-})
+	formCreateTaskBlock.append(spanErrorText)
+}
+
+
 
 const modalWindow = document.querySelector('.modal-overlay')
 const modalCancelButton = modalWindow.querySelector('.delete-modal__cancel-button')
 const modalConfirmButton = modalWindow.querySelector('.delete-modal__confirm-button')
 
-let taskDelete
 let taskDeleteId
+
 
 taskList.addEventListener('click', e => {
 	spanErrorText.remove()
 	const { target } = e
 
 	if (target.classList.contains('delete-button')) {
-		taskDelete = target.closest('.task-item')
+		const taskDelete = target.closest('.task-item')
 		taskDeleteId = taskDelete.getAttribute('data-task-id')
 
-		modalWindow.classList.remove('modal-overlay_hidden')
+		showModalWindow()
 	}
 })
+
+function showModalWindow() {
+	modalWindow.classList.remove('modal-overlay_hidden')
+}
+function hideModalWindow() {
+	modalWindow.classList.add('modal-overlay_hidden')
+}
+
 
 
 modalWindow.addEventListener('click', e => {
+
 	const { target } = e
 
 	if (target === modalCancelButton) {
-		modalWindow.classList.add('modal-overlay_hidden')
+		hideModalWindow()
 	} else if (target === modalConfirmButton) {
-		tasks.forEach((item, index) => {
-			if (item.id === taskDeleteId) {
-				tasks.splice(index, 1)
-			}
-		})
-		taskDelete.remove()
-		modalWindow.classList.add('modal-overlay_hidden')
+		tasks = tasks.filter(task => task.id !== taskDeleteId)
+		showTask()
+		hideModalWindow()
 	}
 })
 
 
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', event => {
 
-	const { key } = e
+	event.preventDefault()
+
+	//! Марк, привет!
+	//? Я немного не понял, если я убираю дефолтное поведение, то
+	//? у меня не получается напечатать новую задачу в инпут.
+	//? Как поступить в этом случае?
+
+	const { key } = event
 
 	if (key === 'Tab') {
 		darkTheme = !darkTheme
